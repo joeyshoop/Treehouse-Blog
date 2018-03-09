2f46fd7ea5caff5820fc3816379140370c1856af
joeyshoop <joeyshoop@gmail.com>
2018-03-08 20:29:27 -0500

    Scraping title and text from page

--- blogpost.rb
+++ blogpost.rb
@@ -3,3 +3,8 @@ require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :firefox
 driver.get('http://blog.teamtreehouse.com/')
 driver.find_element(:link, "Continue reading").click()
+title = driver.find_element(:class, "post-title").text
+copy = driver.find_element(:class, "skinny-read").text
+puts title
+puts ""
+puts copy
