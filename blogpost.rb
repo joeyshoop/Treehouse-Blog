d40bc0bf790c12795724b55010c65fe57a5960e0
joeyshoop <joeyshoop@gmail.com>
2018-03-08 20:35:11 -0500

    Write contents of post to new text file

--- blogpost.rb
+++ blogpost.rb
@@ -5,6 +5,8 @@ driver.get('http://blog.teamtreehouse.com/')
 driver.find_element(:link, "Continue reading").click()
 title = driver.find_element(:class, "post-title").text
 copy = driver.find_element(:class, "skinny-read").text
-puts title
-puts ""
-puts copy
+file = File.open("blogpost.txt", "w")
+file.puts title
+file.puts ""
+file.puts copy
+file.close
